2dc83f96285aa710c14d9fff88daf15cf80b2480
Romit Chaudhari <romitchaudhari@Romits-MacBook-Pro.local>
2020-01-14 21:36:15 -0500

    Updates

---
+++ precache-manifest.43a8776a85a9d4331afd5eeaf0988ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556c30afe9dda17cf4474de7426577b1",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "e92cae7ac9819e0edfec",
+    "url": "/Clicky-Game/static/css/main.836f42eb.chunk.css"
+  },
+  {
+    "revision": "24174d323ac21f04f040",
+    "url": "/Clicky-Game/static/js/2.49212cf7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.49212cf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e92cae7ac9819e0edfec",
+    "url": "/Clicky-Game/static/js/main.b89ddb56.chunk.js"
+  },
+  {
+    "revision": "46124c1ee5b410f9337d",
+    "url": "/Clicky-Game/static/js/runtime-main.a63a661f.js"
+  }
+]);
